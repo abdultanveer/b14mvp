207e1d778c0dd4fb32367b4868c9743b3c7ddd15
ansari <abdul.tanveer@gmail.com>
2018-09-11 20:51:54 +0530

    default way of handling button click

--- app/src/main/java/b14/rjt/com/myapplication/ui/main/MainActivity.java
+++ app/src/main/java/b14/rjt/com/myapplication/ui/main/MainActivity.java
@@ -3,6 +3,7 @@ package b14.rjt.com.myapplication.ui.main;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -12,7 +13,7 @@ import b14.rjt.com.myapplication.R;
 /**
  * here mainactivity is my view
  */
-public class MainActivity extends AppCompatActivity implements IView {
+public class MainActivity extends AppCompatActivity implements IView, View.OnClickListener {
 
     IPresenter presenter;
     @Override
@@ -20,19 +21,21 @@ public class MainActivity extends AppCompatActivity implements IView {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         presenter = new Presenter(this);
+        Button mButton = findViewById(R.id.changeTextBt);
+        mButton.setOnClickListener(this);
     }
 
-    public void clickHandler(View view) {
+   /* public void clickHandler(View view) {
         //let my view tell the presenter that an event [button click]
         // happened to the presenter
         presenter.onButtonClicked(view);
 
-        /*EditText editText = findViewById(R.id.editTextUserInput);
+        *//*EditText editText = findViewById(R.id.editTextUserInput);
         String name = editText.getText().toString();
         TextView result = findViewById(R.id.textToBeChanged);
-        result.setText(name);*/
+        result.setText(name);*//*
     }
-
+*/
 
     @Override
     public void showToast() {
@@ -40,4 +43,8 @@ public class MainActivity extends AppCompatActivity implements IView {
     }
 
 
+    @Override
+    public void onClick(View view) {
+        presenter.onButtonClicked(view);
+    }
 }
